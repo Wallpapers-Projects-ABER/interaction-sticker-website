4664b6cd09bd822230f0a5395e799344516fb437
ABER1047 <78880883+ABER1047@users.noreply.github.com>
2022-09-12 05:40:20 +0900

    2022_09_12
    added custom background system
    changed sticker size
    etc.

--- scripts.js
+++ scripts.js
@@ -10,10 +10,10 @@ $(window).resize(function()
 c_w = view_width/1920;
 })
 //image_values
-var imgs_obj = [], imgs_clicked = [], imgs_num = -1, direction = 0, n_dir = 1, t_dir = 0, can_place = false, remover_activated = -1, uploaded_file_target = 0;
-var background_img = -4;
+var imgs_obj = [], imgs_clicked = [], imgs_num = -1, direction = 0, n_dir = 1, t_dir = 0, can_place = false, remover_activated = -1, already_exists_background = false;
+var background_img = -4, random_bg_n = -4;
 var random_sticker = ["chinanako","sakana","chisato1","chisato2","deto","takina1"];//["ame","aqua","calli","chinanako","sakana","chisato1","chisato2","deto","takina1"];
-
+var random_sticker_2 = ["elaina1","elaina2","elaina3","elaina4"];
 
 
 //set values for css
@@ -141,15 +141,68 @@ document.querySelector(".random_sticker").style.transition = "width 0s";
 document.querySelector(".random_sticker").style.width = "0px";
 })
 
+
+
+
+
+
+//random background text animation
+document.querySelector(".random_background").addEventListener("mouseover",function()
+{
+document.querySelector(".random_background").style.color = "#ff5647";
+document.querySelector(".random_background").style.background= "white";
+document.querySelector(".random_background").style.transition = "width 0.6s";
+document.querySelector(".random_background").style.width = "320px";
+})
+
+document.querySelector(".random_background").addEventListener("mouseleave",function()
+{
+document.querySelector(".random_background").style.color = "black";
+document.querySelector(".random_background").style.background = "none";
+document.querySelector(".random_background").style.transition = "width 0s";
+document.querySelector(".random_background").style.width = "0px";
+})
+
+document.querySelector(".random_background").addEventListener("click",function()
+{
+    if (already_exists_background == true)
+    {
+    background_img.remove();
+    }
+
+var random_bg = (Math.random()*5 | 0);
+if (random_bg == 0)
+{
+random_bg = 4;
+}
+random_bg_n = random_bg;
+console.log("random_bg"+random_bg);
+
+background_img = document.createElement("video");
+background_img.src = "videos/wallpaper_background"+random_bg+".mp4"
+background_img.autoplay = true;
+background_img.controls = false;
+background_img.loop = true;
+background_img.style.width = view_width+"px";
+background_img.style.zIndex = 1;
+background_img.style.position = "absolute";
+background_img.style.top = 0;
+background_img.style.left = 0;
+background_img.draggable = false;
+background_img.style.display = "block";
+background_img.style.opacity = "1";
+$("input").after(background_img);
+
+already_exists_background = true;
+})
+
 //check file selected and upload automatically
 function check_file()
 {
 var file_value = document.getElementById("file_selector").value;
     if (file_value != "")
     {
-    uploaded_file_target = 0;
     clearTimeout(check_file_timer);
-    document.getElementById("file_selector").value = "";
     }
     else
     {
@@ -164,8 +217,6 @@ var bg_file_value = document.getElementById("bg_file_selector").value;
     if (bg_file_value != "")
     {
     clearTimeout(check_bg_file_timer);
-    uploaded_file_target = 1;
-    document.getElementById("bg_file_selector").value = "";
     }
     else
     {
@@ -176,68 +227,79 @@ var bg_file_value = document.getElementById("bg_file_selector").value;
 //add image sticker
 $("input").change(function(e)
 {
-console.log("input"+uploaded_file_target);
+var input_file = document.getElementById("file_selector").value;
+var input_file_bg = document.getElementById("bg_file_selector").value;
+console.log("input");
     for(var i = 0; i < e.originalEvent.srcElement.files.length; i++) 
     {
     var file = e.originalEvent.srcElement.files[i];
     }
     
-    //sticker
-    if (uploaded_file_target == 0)
-    {
-    imgs_num ++;
-    imgs_obj[imgs_num] = document.createElement("img");
-    imgs_obj[imgs_num].style.cursor = "pointer";
-    imgs_obj[imgs_num].style.width = "310px";
-    imgs_obj[imgs_num].style.position = "absolute";
-    imgs_obj[imgs_num].style.display = "block";
-    imgs_obj[imgs_num].style.opacity = "0.4";
-    imgs_obj[imgs_num].style.transition = "transform 0.1s";
-    imgs_obj[imgs_num].style.zIndex = imgs_num;
-    imgs_obj[imgs_num].draggable = false;
-    get_mouse_dir(imgs_num);
-    setTimeout(placalble_now,100);
-    //var check_click2 = setTimeout(interacting_now,300)
-    imgs_clicked[imgs_num] = 1;
-    }
-    else
-    {
-        if (target_src.includes(".mp4") || target_src.includes(".avi"))
-        {
-        background_img = document.createElement("video");
-        }
-        else
-        {
-        background_img = document.createElement("img");
-        }
-    background_img.width = view_width;
-    background_img.zIndex = 1;
-    background_img.style.position = "absolute";
-    background_img.autoplay = true;
-    }
-
 var reader = new FileReader();
     reader.onloadend = function() 
     {
-        if (uploaded_file_target == 0)
+        if (input_file != "")
         {
         imgs_obj[imgs_num].src = reader.result;
         }
         else
         {
-        background_img.source.scr = reader.result;
+        background_img.src = reader.result;
         }
     }
 reader.readAsDataURL(file);
-
-    if (uploaded_file_target == 0)
+console.log("file"+reader.result);
+    //sticker
+    if (input_file != "")
     {
+    imgs_num ++;
+    imgs_obj[imgs_num] = document.createElement("img");
+    imgs_obj[imgs_num].style.cursor = "pointer";
+    imgs_obj[imgs_num].style.width = "200px";
+    imgs_obj[imgs_num].style.position = "absolute";
+    imgs_obj[imgs_num].style.display = "block";
+    imgs_obj[imgs_num].style.opacity = "0.4";
+    imgs_obj[imgs_num].style.transition = "transform 0.1s";
+    imgs_obj[imgs_num].style.zIndex = imgs_num+10;
+    imgs_obj[imgs_num].draggable = false;
+    get_mouse_dir(imgs_num);
+    setTimeout(placalble_now,100);
+    //var check_click2 = setTimeout(interacting_now,300)
+    imgs_clicked[imgs_num] = 1;
     $("input").after(imgs_obj[imgs_num]);
     }
     else
     {
+        if (already_exists_background == true)
+        {
+        background_img.remove();
+        }
+    
+        if (input_file_bg.includes(".mp4") || input_file_bg.includes(".avi"))
+        {
+        background_img = document.createElement("video");
+        background_img.autoplay = true;
+        background_img.controls = false;
+        background_img.loop = true;
+        console.log("video");
+        }
+        else
+        {
+        background_img = document.createElement("img");
+        }
+    background_img.style.width = view_width+"px";
+    background_img.style.zIndex = 1;
+    background_img.style.position = "absolute";
+    background_img.style.top = 0;
+    background_img.style.left = 0;
+    background_img.draggable = false;
+    background_img.style.display = "block";
+    background_img.style.opacity = "1";
     $("input").after(background_img);
     }
+document.getElementById("file_selector").value = "";
+document.getElementById("bg_file_selector").value = "";
+already_exists_background = true;
 })
 
 
@@ -271,8 +333,8 @@ mouse_y = event.clientY;
     {
         if (imgs_clicked[ii] == 1)
         {
-        imgs_obj[ii].style.left = mouse_x-150+"px";
-        imgs_obj[ii].style.top = mouse_y-150+"px";
+        imgs_obj[ii].style.left = mouse_x-100+"px";
+        imgs_obj[ii].style.top = mouse_y-100+"px";
         }
     }
 })
@@ -361,22 +423,31 @@ document.querySelector(".random_sticker").addEventListener("click",function()
 {
 document.querySelector(".random_sticker").style.color = "black";
 
-var random_value = Math.floor(Math.random()*random_sticker.length | 0);
+
 imgs_num ++;
 imgs_obj[imgs_num] = document.createElement("img");
 imgs_obj[imgs_num].style.cursor = "pointer";
-imgs_obj[imgs_num].style.width = "310px";
+imgs_obj[imgs_num].style.width = "200px";
 imgs_obj[imgs_num].style.position = "absolute";
 imgs_obj[imgs_num].style.display = "block";
 imgs_obj[imgs_num].style.opacity = "0.4";
 imgs_obj[imgs_num].style.transition = "transform 0.1s";
-imgs_obj[imgs_num].style.zIndex = imgs_num;
+imgs_obj[imgs_num].style.zIndex = imgs_num+10;
 imgs_obj[imgs_num].style.left = "-999px";
 imgs_obj[imgs_num].draggable = false;
 setTimeout(get_mouse_dir,100,imgs_num);
 can_place = false;
 setTimeout(placalble_now,100);
 imgs_clicked[imgs_num] = 1;
+if (random_bg_n == 2 || random_bg_n == 4)
+{
+var random_value = Math.floor(Math.random()*random_sticker_2.length | 0);
+imgs_obj[imgs_num].src = "imgs/"+random_sticker_2[random_value]+".png";
+}
+else
+{
+var random_value = Math.floor(Math.random()*random_sticker.length | 0);
 imgs_obj[imgs_num].src = "imgs/"+random_sticker[random_value]+".png";
+}
 $("input").after(imgs_obj[imgs_num]);
 })
